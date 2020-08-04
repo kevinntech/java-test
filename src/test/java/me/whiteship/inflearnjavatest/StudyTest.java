3ded87f2225cc7445bd61cfc84bc8b7e19f98b10
kevinntech <kevinntech@nate.com>
2020-08-04 22:11:16 +0900

    JUnit5 Assertion

--- src/test/java/me/whiteship/inflearnjavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnjavatest/StudyTest.java
@@ -1,11 +1,15 @@
 package me.whiteship.inflearnjavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -13,42 +17,45 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 ☆")
     void create_new_study(){
-        //Study study = new Study(-10);
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println(test_env);
 
-        /*
-        assertNotNull(study);   // study 인스턴스가 null이 아닌지 검증
-        assertEquals(StudyStatus.DRAFT, study.getStatus(),
-                () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다.");
-        // 람다식으로 메시지를 만들면 테스트가 실패 했을 때만 문자열 연산을 함 (람다식을 쓰면 성능 상 유리)
-        // 람다식을 이용하지 않으면 문자열 연산은 테스트 성공 여부에 관계 없이 항상 진행 됨
-        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0 보다 커야 한다.");
+        //assumeTrue("LOCAL".equalsIgnoreCase(test_env));
 
-        */
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println("local");
+            Study actual = new Study(100);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        }); // 조건을 만족하는 경우 테스트 실행
 
-        /*
-        assertAll(
-                () -> assertNotNull(study),
-                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
-                        () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다."),
-                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0 보다 커야 한다.")
-        );
+        assumingThat("keesun".equalsIgnoreCase(test_env), () -> {
+            System.out.println("keesun");
+            Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        }); // 조건을 만족하는 경우 테스트 실행
 
-         */
-
-        /*
-        IllegalArgumentException exception =
-                assertThrows(IllegalArgumentException.class, () -> new Study(-10));
-        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
-
-         */
-
-        // assertTimeoutPreemptively
-        assertTimeout(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);
-        });
-        // TODO ThreadLocal
     }
+
+    // @EnabledOnJre({JRE.JAVA_11, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11})
+    // @EnabledIfEnvironmentVariable(named = "TEST_ENV" , matches = "LOCAL")
+    /*
+    @Test
+    @DisplayName("스터디 만들기 ☆")
+    @EnabledOnOs({OS.MAC, OS.LINUX})
+    void create_new_study2() {
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println("local");
+        Study actual = new Study(100);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("스터디 만들기 ★")
+    @DisabledOnOs(OS.MAC)
+    void create_new_study_again2(){
+        System.out.println("create1");
+    }
+    */
 
     @Test
     @DisplayName("스터디 만들기 ★")
