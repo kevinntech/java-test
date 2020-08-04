504ef8a390db14410ca183b6b2df413318de4f1c
kevin <kevinntech@nate.com>
2020-08-04 23:41:25 +0900

    태깅과 필터링

--- src/test/java/me/whiteship/inflearnjavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnjavatest/StudyTest.java
@@ -15,50 +15,16 @@ import static org.junit.jupiter.api.Assumptions.assumingThat;
 class StudyTest {
 
     @Test
-    @DisplayName("스터디 만들기 ☆")
+    @DisplayName("스터디 만들기 fast")
+    @Tag("fast")
     void create_new_study(){
-        String test_env = System.getenv("TEST_ENV");
-        System.out.println(test_env);
-
-        //assumeTrue("LOCAL".equalsIgnoreCase(test_env));
-
-        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
-            System.out.println("local");
-            Study actual = new Study(100);
-            assertThat(actual.getLimit()).isGreaterThan(0);
-        }); // 조건을 만족하는 경우 테스트 실행
-
-        assumingThat("keesun".equalsIgnoreCase(test_env), () -> {
-            System.out.println("keesun");
-            Study actual = new Study(10);
-            assertThat(actual.getLimit()).isGreaterThan(0);
-        }); // 조건을 만족하는 경우 테스트 실행
-
-    }
-
-    // @EnabledOnJre({JRE.JAVA_11, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11})
-    // @EnabledIfEnvironmentVariable(named = "TEST_ENV" , matches = "LOCAL")
-    /*
-    @Test
-    @DisplayName("스터디 만들기 ☆")
-    @EnabledOnOs({OS.MAC, OS.LINUX})
-    void create_new_study2() {
-        String test_env = System.getenv("TEST_ENV");
-        System.out.println("local");
-        Study actual = new Study(100);
+        Study actual = new Study(10);
         assertThat(actual.getLimit()).isGreaterThan(0);
     }
 
     @Test
-    @DisplayName("스터디 만들기 ★")
-    @DisabledOnOs(OS.MAC)
-    void create_new_study_again2(){
-        System.out.println("create1");
-    }
-    */
-
-    @Test
-    @DisplayName("스터디 만들기 ★")
+    @DisplayName("스터디 만들기 slow")
+    @Tag("slow")
     void create_new_study_again(){
         System.out.println("create1");
     }
