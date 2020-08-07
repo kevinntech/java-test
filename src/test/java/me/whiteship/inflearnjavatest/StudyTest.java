9c3af1b42518c4f1dddf3a75849655118c5c77d8
kevinntech <kevinntech@nate.com>
2020-08-07 22:16:15 +0900

    테스트 반복하기 1부

--- src/test/java/me/whiteship/inflearnjavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnjavatest/StudyTest.java
@@ -2,6 +2,8 @@ package me.whiteship.inflearnjavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -25,6 +27,23 @@ class StudyTest {
     @DisplayName("스터디 만들기 slow")
     void create_new_study_again(){
         System.out.println("create1");
+    }
+
+    // value : 반복 횟수, name : 반복 테스트 이름
+    // currentRepetition : 현재 반복 횟수, totalRepetitions : 전체 반복 횟수
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition} / {totalRepetitions} " )
+    void repeatTest(RepetitionInfo repetitionInfo){
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    // 여러 다른 매개변수를 대입 해가며 반복적으로 테스트를 실행한다.
+    // 아래와 같은 문자열 4개를 반복적으로 테스트하는 코드를 작성한다.
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(strings = {"날씨가", "많이" , "추워지고", "있네요."} )
+    void parameterizeTest(String message){ // 저 4개의 문자열을 각각 하나씩 받아주는 파라미터 정의
+        System.out.println(message);
     }
 
     @BeforeAll
